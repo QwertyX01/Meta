3c0c7e1bd1777fffd4f87ffdff3fa2d86ff97855
ScripterZenin <loksiq1201@gmail.com>
2026-09-04 20:33:25 +0500

    Update Meta.lua

--- Meta.lua
+++ Meta.lua
@@ -1850,3 +1850,5 @@ UserInputService.InputBegan:Connect(function(input, gameProcessed)
         MainFrame.Visible = not MainFrame.Visible
     end
 end)
+print("[META] META v7.0.40 - Health Bar Right Shoulder")
+print("[META] Press Insert or click icon")
